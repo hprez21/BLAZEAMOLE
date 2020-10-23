aa6d816b90bb7d6291a08edff23ab7600d9bb65e
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2020-10-23 09:12:24 -0500

    First Commit
    First Commit, based on live video:
    
    https://www.youtube.com/watch?v=KYvndfVIdE4&feature=youtu.be

---
+++ BlazeAMoleDemo/Models/SquareModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazeAMoleDemo.Models
+{
+    public class SquareModel
+    {
+        private bool isShown;
+
+        public int Id { get; set; }
+        public string Style { get; set; }
+        public bool IsShown
+        {
+            get => isShown;
+            set
+            {
+                isShown = value;
+                if (isShown)
+                {
+                    Style = "mole";
+                    Console.WriteLine($"Se cambió a mole {Id}");
+                }
+                else
+                {
+                    Style = "";
+                }
+            }
+        }
+    }
+
+}
---
+++ BlazeAMoleDemo/Pages/BlazeAMole.cs
@@ -0,0 +1,75 @@
+﻿using BlazeAMoleDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazeAMoleDemo.Pages
+{
+    public partial class BlazeAMole
+    {
+        private int score = 0;
+        private int currentTime = 60;
+        int hitPosition = 0;
+        private string message = "";
+        bool gameIsRunning = true;
+
+        public List<SquareModel> Squares { get; set; } =
+            new List<SquareModel>();
+
+        public BlazeAMole()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Squares.Add(new SquareModel
+                {
+                    Id = i
+                });
+            }
+        }
+
+
+        private void MouseUp(SquareModel s)
+        {
+            if (s.Id == hitPosition)
+            {
+                score += 1;
+            }
+        }
+
+        private void ChooseSquare()
+        {
+            foreach (var item in Squares)
+            {
+                item.IsShown = false;
+            }
+            var randomPosition = new Random().Next(0, 9);
+            Squares[randomPosition].IsShown = true;
+            Console.WriteLine(randomPosition);
+            hitPosition = randomPosition;
+            StateHasChanged();
+        }
+
+        private async Task GameLoop()
+        {
+            while (gameIsRunning)
+            {
+
+                currentTime--;
+                if (currentTime == 0)
+                {
+                    message = "EL JUEGO FINALIZÓ!";
+                    gameIsRunning = false;
+                }
+                ChooseSquare();
+                await Task.Delay(1000);
+            }
+        }
+
+        protected override async void OnInitialized()
+        {
+            await GameLoop();
+        }
+
+    }
+}
